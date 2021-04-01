0a4cb3520c117d091c7f3880bbfcfb23213b7112
Marko Lazovic <marko.lazovic@gmx.com>
2021-04-01 11:28:52 +0200

    First working versio with json dummy data

--- clients-app/src/App.js
+++ clients-app/src/App.js
@@ -1,12 +1,45 @@
+import React, { Component } from 'react';
 import './App.css';
-import { Button } from '@material-ui/core';
+import Container from '@material-ui/core/Container';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
-function App() {
-  return (
-    <div className="App">
-      <Button variant="contained" color="primary">Hello World</Button>
-    </div>
-  );
+import data from './json/clients.json';
+
+// import axios from 'axios';
+
+class App extends Component {
+  state = {
+    clients: data
+  }
+
+
+
+  render() {    
+    const clients = [...this.state.clients.clients];
+    let clientsListItems = '';
+    if (clients) {
+      clientsListItems = clients.map(client => {
+        return(
+          <ListItem button key={client.id}>
+            <ListItemText primary={`${client.firstName} ${client.lastName}`} />
+          </ListItem>
+        );
+      })
+    }
+  
+    return (
+      <div className="App">
+        <Container fixed>
+          <h1>Clients App</h1>
+          <List component="nav">
+            {clientsListItems}
+          </List>
+        </Container>
+      </div>
+    );
+  }  
 }
 
 export default App;
